3f2844dba5a378621814c6bb0efa4fc0442ee69c
suryabk <surya27bk@gmail.com>
2024-04-27 03:16:35 +0700

    delete deadline

--- database/sql_migrations/1_initiate.sql
+++ database/sql_migrations/1_initiate.sql
@@ -2,7 +2,7 @@
 -- +migrate StatementBegin
 
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -10,27 +10,27 @@ CREATE TABLE users (
 );
 
 CREATE TABLE task_status (
-    status_id SERIAL PRIMARY KEY,
+    status_id SERIAL PRIMARY KEY NOT NULL,
     status_name VARCHAR(20) NOT NULL UNIQUE
 );
 
 CREATE TABLE projects (
-    project_id SERIAL PRIMARY KEY,
+    project_id SERIAL PRIMARY KEY NOT NULL,
     project_name VARCHAR(100) NOT NULL,
     description TEXT,
     budget DECIMAL(10, 2),
-    deadline DATE,
+    -- deadline DATE,
     created_by INTEGER REFERENCES users(user_id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE tasks (
-    task_id SERIAL PRIMARY KEY,
+    task_id SERIAL PRIMARY KEY NOT NULL,
     task_name VARCHAR(100) NOT NULL,
     description TEXT,
     priority VARCHAR(20),
-    deadline DATE,
+    -- deadline DATE,
     status_id INTEGER REFERENCES task_status(status_id), 
     assigned_to INTEGER REFERENCES users(user_id),
     project_id INTEGER REFERENCES projects(project_id),
